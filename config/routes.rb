0c3590cc983a704f409ffaff5414212a0e8e4517
ChunkySquirrel <matejmobile516@gmail.com>
2020-06-08 00:37:57 +0200

    osnovno

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/about'
   resources :posts
 
   root to: 'posts#index'
+  get "about" => 'home#about'
+  get "contact" => 'home#contact'
+  get "achievements" => 'home#achievements'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
